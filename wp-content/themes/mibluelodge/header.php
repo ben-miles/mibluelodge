6d358f7f510e8e2c72b204504e2273d1b1fa7b80
Ben Miles <bcgm3studios@gmail.com>
2024-01-17 11:41:37 -0500

    Update theme paths 2

--- wp-content/themes/mibluelodge/header.php
+++ wp-content/themes/mibluelodge/header.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Web Design & Development Portfolio of Ben Miles">
     <meta name="author" content="Ben Miles">
-    <link rel="icon" href="/wp-content/themes/benmiles-wp/img/ben-miles_favicon.png">
+    <link rel="icon" href="/wp-content/themes/mibluelodge/img/ben-miles_favicon.png">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>

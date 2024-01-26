c2cf7729e5a4e59c70296e1046fab5ea2b72c854
Ben Miles <bcgm3studios@gmail.com>
2024-01-26 11:03:56 -0500

    MVP Hero Section

--- wp-content/themes/mibluelodge/page-home.php
+++ wp-content/themes/mibluelodge/page-home.php
@@ -4,9 +4,9 @@ Template Name: Home
 */
 
 get_header(null, ['bodyClass' => 'page-home']);
-include('section-intro.php');
-include('section-about.php');
-include('section-portfolio-featured.php');
-include('section-contact.php');
+include('section-hero.php');
+// include('section-about.php');
+// include('section-portfolio-featured.php');
+// include('section-contact.php');
 get_footer();
 ?>
---
+++ wp-content/themes/mibluelodge/section-hero.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+Template Name: Section - Hero
+*/
+// error_reporting(0);
+?>
+
+<!-- hero -->
+<section id="hero">
+    <div class="container">
+    	<div class="row">
+			<div class="col-12">
+				<h1 class="title"><?php echo get_bloginfo('name'); ?></h1>
+				<h2 class="subtitle"><?php echo get_bloginfo('description'); ?></h2>
+			</div>
+		</div>
+    </div>
+</section>
+<!-- / hero -->

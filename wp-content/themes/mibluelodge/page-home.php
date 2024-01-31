5030088fadec437d5deb967804c63f1393d24889
Ben Miles <bcgm3studios@gmail.com>
2024-01-30 22:29:40 -0500

    MVP Calendar Section

--- wp-content/themes/mibluelodge/page-home.php
+++ wp-content/themes/mibluelodge/page-home.php
@@ -7,6 +7,6 @@ get_header(null, ['bodyClass' => 'page-home']);
 include('section-hero.php');
 include('section-wm-message.php');
 include('section-social-media.php');
-// include('section-contact.php');
+include('section-calendar.php');
 get_footer();
 ?>
---
+++ wp-content/themes/mibluelodge/section-calendar.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+Template Name: Section - Calendar
+*/
+// error_reporting(0);
+?>
+
+<!-- hero -->
+<section id="calendar">
+    <div class="container">
+    	<div class="row">
+			<div class="col-12 text-center">
+				<h1 class="title">Calendar</h1>
+				<h2 class="subtitle">Important Notices & Events</h2>
+				<iframe src="https://calendar.google.com/calendar/embed?title=Merritt%20Island%20Lodge%20No.%20353%20F%26AM%20-%20EVENT%20CALENDAR&amp;height=600&amp;wkst=1&amp;bgcolor=%233366ff&amp;src=mibluelodge353%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FNew_York" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+			</div>
+		</div>
+    </div>
+</section>
+<!-- / hero -->

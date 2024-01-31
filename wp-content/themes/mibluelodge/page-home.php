a5a11657ea1eca582f761befc12301e6e131531f
Ben Miles <bcgm3studios@gmail.com>
2024-01-30 21:12:46 -0500

    MVP Worshipful Master's Message Section

--- wp-content/themes/mibluelodge/page-home.php
+++ wp-content/themes/mibluelodge/page-home.php
@@ -5,7 +5,7 @@ Template Name: Home
 
 get_header(null, ['bodyClass' => 'page-home']);
 include('section-hero.php');
-// include('section-about.php');
+include('section-wm-message.php');
 // include('section-portfolio-featured.php');
 // include('section-contact.php');
 get_footer();

9fb43a704ddc1dcf22301696851070ae50c3e3a1
Ben Miles <bcgm3studios@gmail.com>
2024-02-04 11:42:19 -0500

    MVP What is a Mason Section

--- wp-content/themes/mibluelodge/page-home.php
+++ wp-content/themes/mibluelodge/page-home.php
@@ -8,5 +8,6 @@ include('section-hero.php');
 include('section-wm-message.php');
 include('section-social-media.php');
 include('section-calendar.php');
+include('section-what-is-a-mason.php');
 get_footer();
 ?>
---
+++ wp-content/themes/mibluelodge/section-what-is-a-mason.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+Template Name: Section - What is a Mason?
+*/
+// error_reporting(0);
+?>
+
+<!-- hero -->
+<section id="what-is-a-mason">
+    <div class="container">
+    	<div class="row">
+			<div class="col-6">
+				<h1 class="title">Start Your Journey</h1>
+				<h2 class="subtitle">What is a Freemason?</h2>
+				<p>Freemasonry is the oldest and largest fraternity in the world. Its members share a common goal of helping each other.</p>
+				<div class="buttons">
+					<button type="button" class="btn btn-primary">Learn More</button>
+				</div>
+			</div>
+		</div>
+    </div>
+</section>
+<!-- / hero -->

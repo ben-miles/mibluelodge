cff886e1a6ada9a30c1be54965302dd37c2f5f5c
Ben Miles <bcgm3studios@gmail.com>
2024-03-16 17:55:03 -0400

    Rename file for WM's Message section template
    Just want to stick to more verbose titles / avoid abbreviations.

--- wp-content/themes/mibluelodge/page-home.php
+++ wp-content/themes/mibluelodge/page-home.php
@@ -5,7 +5,7 @@ Template Name: Home
 
 get_header(null, ['bodyClass' => 'page-home']);
 include('section-hero.php');
-include('section-wm-message.php');
+include('section-worshipful-masters-message.php');
 include('section-social-media.php');
 include('section-calendar.php');
 include('section-what-is-a-mason.php');

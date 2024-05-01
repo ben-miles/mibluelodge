0b132b9d51dbae07e75c3510a2a9e4b406ab2138
Ben Miles <bcgm3studios@gmail.com>
2024-05-01 08:13:31 -0400

    MVP Custom Block Plugin: Copyright Date Block

--- wp-content/plugins/copyright-date-block/build/index.asset.php
+++ wp-content/plugins/copyright-date-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f89a91df0eec95d194f8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '94a61cdf60b87c12c077');
--- wp-content/plugins/copyright-date-block/src/edit.js
+++ wp-content/plugins/copyright-date-block/src/edit.js
@@ -14,14 +14,6 @@ import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 
 /**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
-
-/**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
  *
@@ -30,12 +22,8 @@ import './editor.scss';
  * @return {Element} Element to render.
  */
 export default function Edit() {
+	const currentYear = new Date().getFullYear().toString();
 	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Copyright Date Block – hello from the editor!',
-				'copyright-date-block'
-			) }
-		</p>
+		<p { ...useBlockProps() }>© { currentYear }</p>
 	);
 }

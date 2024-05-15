f2bd202bd7301012a0068e9a76b2e2b44d52abb4
Ben Miles <bcgm3studios@gmail.com>
2024-05-15 12:17:50 -0400

    MVP Plugin: Heading with Accent
    Working on another custom block plugin, this time for the stylized headings I want. The vanilla Gutenberg/block editor doesn't allow for applying some of the customizations I'd need (negative margins, most importantly).

---
+++ wp-content/plugins/heading-with-accent/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '20f4370de3895bb47c8e');
---
+++ wp-content/plugins/heading-with-accent/src/edit.js
@@ -0,0 +1,56 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { PanelBody, TextControl } from '@wordpress/components';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+export default function Edit( { attributes, setAttributes } ) {
+	const { headingNormal, headingAccented } = attributes;
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings', 'heading-with-accent' ) }>
+					<TextControl
+						label={ __(
+							'Heading Normal',
+							'heading-with-accent'
+						) }
+						value={ headingNormal || '' }
+						onChange={ ( value ) =>
+							setAttributes( { headingNormal: value } )
+						}
+					/>
+					<TextControl
+						label={ __(
+							'Heading Accent',
+							'heading-with-accent'
+						) }
+						value={ headingAccented || '' }
+						onChange={ ( value ) =>
+							setAttributes( { headingAccented: value } )
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<h3 { ...useBlockProps() }>{ headingNormal }<em>{ headingAccented }</em></h3>
+		</>
+	);
+}

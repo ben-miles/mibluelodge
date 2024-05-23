bd03d394033eba289431f5480b98ff9a06bd6697
Ben Miles <bcgm3studios@gmail.com>
2024-05-23 16:44:41 -0400

    Accented Heading Block: Update Display Name

--- wp-content/plugins/accented-heading-block/build/index.asset.php
+++ wp-content/plugins/accented-heading-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '40c62a21448813e13eaa');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9ab487a6c0f6ec119ff6');

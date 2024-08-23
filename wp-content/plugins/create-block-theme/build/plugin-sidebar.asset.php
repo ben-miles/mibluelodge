5385a2ab22399adf2c3733483325b67079f935fe
Ben Miles <bcgm3studios@gmail.com>
2024-08-23 12:52:00 -0400

    Update Plugin: Create Block Theme
    2.3 -> 2.4

--- wp-content/plugins/create-block-theme/build/plugin-sidebar.asset.php
+++ wp-content/plugins/create-block-theme/build/plugin-sidebar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-site', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-preferences', 'wp-primitives'), 'version' => '1f95afba55ba90679ed8');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-preferences', 'wp-primitives'), 'version' => 'f7c69510bc0951897a84');
